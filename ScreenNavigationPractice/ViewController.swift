3d79b115d2b8a0e3b2bb8dd077bffb1dc6afbe8e
Nigostarr <wlckdgus5833@naver.com>
2020-06-30 13:07:34 +0900

    Initial Commit

---
+++ ScreenNavigationPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenNavigationPractice
+//
+//  Created by Ji Chang Hyun on 2020/06/30.
+//  Copyright © 2020 Nigostarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
