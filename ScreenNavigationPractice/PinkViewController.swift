7e3f51002e47e585218479aff91db2a0509e5118
Nigostarr <wlckdgus5833@naver.com>
2020-06-30 13:47:51 +0900

    #1. Build Screens

---
+++ ScreenNavigationPractice/PinkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PinkViewController.swift
+//  ScreenNavigationPractice
+//
+//  Created by Ji Chang Hyun on 2020/06/30.
+//  Copyright © 2020 Nigostarr. All rights reserved.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pink Screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
